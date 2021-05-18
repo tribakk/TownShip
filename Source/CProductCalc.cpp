f8c78c399b7cf2e2deca4b2b2ca6fc90ae36bca8
Павел Козлов <pavel.kozlov@fsight.ru>
2021-05-18 19:48:50 +0500

    вынес реализации в CPP (кроме CFactory.h, ее нет смысла выносить)

---
+++ Source/CProductCalc.cpp
@@ -0,0 +1,66 @@
+#pragma once
+#include "stdafx.h"
+#include "CProductCalc.h"
+CProductCalc::CProductCalc()
+{
+	m_FactArray.Add(new CMilkFactory());
+	m_FactArray.Add(new CBakery());
+	m_FactArray.Add(new СWeavingFactory());
+	m_FactArray.Add(new CSewingFactory());
+	m_FactArray.Add(new CSnackFactory());
+	m_FactArray.Add(new CFastFoodFactory());
+	m_FactArray.Add(new CRubberFactory());
+	m_FactArray.Add(new CSugarFactory());
+	m_FactArray.Add(new CPaperFactory());
+	m_FactArray.Add(new CIceCreamFactory());
+	m_FactArray.Add(new СconfectioneryFactory());
+
+	SetHaveProduct(m_AlreadyHave);
+}
+
+CProductCalc::~CProductCalc()
+{
+	size_t count = m_FactArray.GetCount();
+	for (size_t i = 0; i < count; i++)
+	{
+		delete m_FactArray[i];
+	}
+}
+void CProductCalc::Add(CString name, int count)
+{
+	m_Array.Add(name, count);
+}
+void CProductCalc::Calc()
+{
+	size_t count = m_FactArray.GetCount();
+	m_Array.ExcludeWhatHave(m_AlreadyHave);
+	//if (firstcalc)
+	//{
+
+	//}
+	for (size_t i = 0; i < count; i++)
+	{
+		CFactory* pFact = m_FactArray[i];
+		pFact->SetInput(m_Array);
+		pFact->Calc();
+		pFact->UpdateResult(m_Array);
+		m_Array.ExcludeWhatHave(m_AlreadyHave);
+	}
+}
+
+void CProductCalc::Print()
+{
+	OutputDebugString(_T("Информация по фабрикам:\n\r"));
+	size_t count = m_FactArray.GetCount();
+	for (int pr = 0; pr < (int)PriorityTag::three + 1; pr++)
+	{
+		for (size_t i = 0; i < count; i++)
+		{
+			if (m_FactArray[i]->GetPriorety() == pr)
+				m_FactArray[i]->Print();
+		}
+
+	}
+	OutputDebugString(_T("\n\rИнформация по ингридиентам:\n\r"));
+	m_Array.Print();
+}
---
+++ Source/CSmartArray.cpp
@@ -0,0 +1,86 @@
+#pragma once
+#include "stdafx.h"
+#include "CSmartArray.h"
+
+void CSmartArray::Add(ProductTag tag, int count)
+{
+	if (count == 0)
+		return;
+	if (m_TagMap.Lookup(tag))
+		m_TagMap[tag] += count;
+	else
+		m_TagMap[tag] = count;
+}
+void CSmartArray::Add(const CString& name, int count)
+{
+	ProductTag tag = GetAllProductSpisok()->GetTag(name);
+	Add(tag, count);
+}
+void CSmartArray::Merge(CSmartArray& Arr)
+{
+	POSITION pos = Arr.m_TagMap.GetStartPosition();
+	for (; pos != NULL;)
+	{
+		ProductCount* prCount = Arr.m_TagMap.GetNext(pos);
+		Add(prCount->m_key, prCount->m_value);
+	}
+}
+void CSmartArray::ClearTag(ProductTag tag)
+{
+	m_TagMap.RemoveKey(tag);
+}
+
+int CSmartArray::GetTagCount(ProductTag tag)
+{
+	int count = 0;
+	if (m_TagMap.Lookup(tag))
+		count = m_TagMap[tag];
+	return count;
+}
+void CSmartArray::RemoveAll()
+{
+	m_TagMap.RemoveAll();
+}
+void CSmartArray::Print()
+{
+	POSITION pos = m_TagMap.GetStartPosition();
+	for (; pos != NULL;)
+	{
+		ProductCount* prCount = m_TagMap.GetNext(pos);
+		CString sCount;
+		sCount.Format(_T(": %i \n\r"), prCount->m_value);
+		OutputDebugString(GetAllProductSpisok()->GetName(prCount->m_key) + sCount);
+	}
+}
+bool CSmartArray::IsEmpty()
+{
+	return m_TagMap.IsEmpty();
+}
+
+void CSmartArray::ExcludeWhatHave(CSmartArray& Arr)
+{
+	POSITION pos = m_TagMap.GetStartPosition();
+	for (; pos != NULL;)
+	{
+		ProductCount* prCount = m_TagMap.GetNext(pos);
+		ProductTag tag = prCount->m_key;
+		if (int count = Arr.GetTagCount(tag))
+		{
+			if (prCount->m_value > count)
+			{
+				Add(tag, -count);
+				Arr.ClearTag(tag);
+			}
+			else if (prCount->m_value < count)
+			{
+				ClearTag(tag);
+				Arr.Add(tag, -prCount->m_value);
+			}
+			else //одинаковое значение
+			{
+				ClearTag(tag);
+				Arr.ClearTag(tag);
+			}
+		}
+	}
+}
